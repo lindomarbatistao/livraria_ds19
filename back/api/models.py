246a6d8b8326bfa96cf19d30bf236dca57da300b
Lindomar <lindomarbatistao@gmail.com>
2025-08-14 07:55:29 -0300

    XX

--- back/api/models.py
+++ back/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Autor(models.Model):
+    nome = models.CharField(max_length=100)
+    s_nome = models.CharField(max_length=100) 
+    nasc = models.DateField(null=True, blank=True)
+    nacio = models.CharField(max_length=50, null=True, blank=True)
+    biogr = models.TextField()
+    
+    
+    
