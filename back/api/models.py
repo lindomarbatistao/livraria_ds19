62a493d9e04a1f1014be6c971066534273d5f487
Lindomar <lindomarbatistao@gmail.com>
2025-08-21 11:38:26 -0300

    XX

--- back/api/models.py
+++ back/api/models.py
@@ -1,11 +1,38 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class Autor(models.Model):
-    nome = models.CharField(max_length=100)
-    s_nome = models.CharField(max_length=100) 
+    autor = models.CharField(max_length=100)
+    s_autor = models.CharField(max_length=100) 
     nasc = models.DateField(null=True, blank=True)
     nacio = models.CharField(max_length=50, null=True, blank=True)
     biogr = models.TextField()
     
+class Editora(models.Model):
+    editora = models.CharField(max_length=100)
+    cnpj = models.CharField(max_length=18, unique=True, null=True, blank=True)    
+    endereco = models.CharField(max_length=200, null=True, blank=True) 
+    telefone = models.CharField(max_length=20, null=True, blank=True) 
+    email = models.EmailField(null=True, blank=True) 
+    site = models.URLField(null=True, blank=True)
+    
+    def __str__(self):
+        return self.editora
     
     
+class Livro(models.Model):
+    titulo = models.CharField(max_length=50)
+    subtitulo = models.CharField(max_length=255)
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
+    editora = models.ForeignKey(Editora, on_delete=models.CASCADE)
+    isbn = models.CharField(max_length=255)
+    descricao = models.TextField()
+    idioma = models.CharField(max_length=100, default="Português")
+    ano = models.IntegerField(validators=[MinValueValidator(1111), MaxValueValidator(9999)])
+    paginas = models.IntegerField()
+    preco = models.DecimalField(max_digits=10, decimal_places=2)
+    estoque = models.IntegerField()
+    desconto = models.DecimalField(max_digits=10, decimal_places=2)
+    disponivel = models.BooleanField(default=True)
+    dimensoes = models.CharField()
+    peso = models.DecimalField(max_digits=10, decimal_places=2)
--- back/api/urls.py
+++ back/api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import AutoresView
+from .views import AutoresView, listar_autores, EditorasView, LivrosView
 
 urlpatterns = [
     path('autores', AutoresView.as_view()),
+    path('authors', listar_autores),
+    path('editoras', EditorasView.as_view()),
+    path('livros', LivrosView.as_view()),
 ]
