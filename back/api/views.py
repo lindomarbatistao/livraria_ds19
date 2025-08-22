2540932e82b09178535626eeb7b3dff5f70ff819
Lindomar <lindomarbatistao@gmail.com>
2025-08-22 11:19:23 -0300

    CRUD

--- back/api/views.py
+++ back/api/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .models import Autor, Editora, Livro
 from .serializers import AutorSerializer, EditoraSerializer, LivroSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
-class AutoresView(ListCreateAPIView):
-    queryset = Autor.objects.all()
-    serializer_class = AutorSerializer
     
 @api_view(['GET', 'POST'])
 def listar_autores(request):
@@ -24,10 +21,32 @@ def listar_autores(request):
         else:
             return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
 
+#################### Autores ###############
+class AutoresView(ListCreateAPIView):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializer
+    
+class AutoresDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializer
+###########################################
+
+#$$$$$$$$$$$$$$$$$$ Editoras $$$$$$$$$$$$$$
 class EditorasView(ListCreateAPIView):
     queryset = Editora.objects.all()
     serializer_class = EditoraSerializer
+
+class EditorasDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Editora.objects.all()
+    serializer_class = EditoraSerializer
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
     
+#%%%%%%%%%%%%%%%% Livros %%%%%%%%%%%%%%%%%
 class LivrosView(ListCreateAPIView):
     queryset = Livro.objects.all()
     serializer_class = LivroSerializer
+
+class LivrosDetailView(RetrieveUpdateDestroyAPIView):
+    queryset = Livro.objects.all()
+    serializer_class = LivroSerializer
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
